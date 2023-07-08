c443fb7471bf1156358d64fb89b006055912d038
Fallen Angel <75208406+sasho4@users.noreply.github.com>
2023-07-09 01:59:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let display = document.querySelector(".display");
+
+
+
+let buttons = Array.from(document.querySelectorAll(".butt1"));
+
+buttons.map((butt1)=>{
+    butt1.addEventListener("click", (e) =>{
+        switch (e.target.innerText){
+            case "AC":
+                display.innerText = "0";
+                break;
+            case "=":
+                try{
+                    display.innerText = eval(display.innerText);
+                }catch (e){
+                    display.innerText = "Ошибка";
+                }
+                break;
+            case "+/-":
+                display.innerText = "-";
+                break;
+            case "%":
+                let passedText = display.innerText + "/100";
+                display.innerText = eval(passedText);
+                break;
+            default:
+                if (display.innerText === "0" && e.target.innerText !== "."){
+                    display.innerText = e.target.innerText;
+                }else {
+                    display.innerText += e.target.innerText;
+                }
+
+        }
+    });
+});
+
